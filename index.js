79d725e89589fec8755b73feb38ac61d009933e3
Ramada Nguyen <ramadawin212@gmail.com>
2022-09-08 19:26:21 -0500

    commit tree d412feaf90b67f183c3a4b37942833b6bb1d138a

--- index.js
+++ index.js
@@ -3,5 +3,31 @@ function destructivelyAppendCat (cat){
     cats.push (cat) 
 }
 function destructivelyPrependCat (cat){
-    cats.push (cat)
+    cats.unshift (cat)
+}
+function destructivelyRemoveLastCat (Garfield){
+    cats.pop (); 
+}
+function  destructivelyRemoveFirstCat (Milo){
+    cats.shift ();
+}
+function appendCat (cat){
+  const copyOfCats = cats.slice() 
+  copyOfCats.push (cat)
+  return copyOfCats
+}
+function prependCat (cat){
+    const copyOfCats = cats.slice()
+    copyOfCats.unshift (cat)
+    return copyOfCats
+}
+function removeLastCat (Garfield){
+    const copyOfCats =  cats.slice()
+    copyOfCats.pop (Garfield)
+    return copyOfCats
+}
+function removeFirstCat (Milo){
+    const copyOfCats = cats.slice()
+    copyOfCats.shift (Milo)
+    return copyOfCats
 }
